e01ed4da94bcbbd31b599be8115b9c10b6fbedf6
Just-D-A <prospeedcuber@gmail.com>
2020-04-20 21:41:52 +0300

    final version

--- src/io/doubleSymbols/DoubleSymbols.java
+++
@@ -1,8 +0,0 @@
-package io.doubleSymbols;
-
-import domain.Token;
-
-public abstract class DoubleSymbols {
-
-    public abstract Token execute(String val, int line, int position) throws Exception;
-}
